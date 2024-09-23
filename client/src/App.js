6ab013dda945de9f571173a8e81fd47feb86d3b0
Sneha Maram <snehamaram@Snehas-MacBook-Pro.local>
2024-09-23 14:08:56 -0500

    upd8

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import profileimg from './profileimg.svg';
 import './App.css';
+
+function TestButton() { //small button at the bottom of the page
+  return (
+    <button>
+    press me !
+    </button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           InfinitePath
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profileimg} className="App-profileimg" alt="profileimg" />
       </header>
+      <TestButton />
     </div>
   );
 }
