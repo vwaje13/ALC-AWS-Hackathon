c269d9a3deb0b07fb1b7ec0a412b081b9215828b
smitheartc <rcs220002@utdallas.edu>
2024-09-27 13:13:44 -0500

    im going to response status

--- backend/db.py
+++ backend/db.py
@@ -6,5 +6,7 @@ load_dotenv()  # loads the .env file which contains db url
 
 def get_db_connection():
     url = os.getenv('DATABASE_URL')  # load the db url
-    connection = psycopg2.connect(url)
+    username = os.getenv('DATABASE_USERNAME')
+    password = os.getenv('DATABASE_PASSWORD')
+    connection = psycopg2.connect(url, user=username, password=password)
     return connection
