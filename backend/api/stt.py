0f503e4eee5191fd838bce12a7af560a757e24ee
risheelg01 <63624190+risheelg01@users.noreply.github.com>
2024-09-27 16:25:00 -0500

    Update stt.py
    fixed audio json input style

--- backend/api/stt.py
+++ backend/api/stt.py
@@ -1,5 +1,5 @@
 import assemblyai as aai
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import jsonify
 import json
 
@@ -9,11 +9,18 @@ class sttApi(Resource):
 
     with open('speechTextApiKey.json', 'r') as file:
         json_data = file.read()
-    parsed_data = json.loads(json_data)
-    aai.settings.api_key = parsed_data['speech_text_api_key']
+        parsed_data = json.loads(json_data)
+        aai.settings.api_key = parsed_data['speech_text_api_key']
+    
+    parser = reqparse.RequestParser()
+    parser.add_argument('audio_path', type=str, required=True, help="Audio path is required")  # either life, social, or academic
+
+    args = parser.parse_args()
+
+    audio_path = str(args['audio_path'])
 
     transcriber = aai.Transcriber()
 
-    transcript = transcriber.transcribe("./my-local-audio-file.mp3")
+    transcript = transcriber.transcribe(audio_path)
 
     return jsonify({"text" : transcript})
